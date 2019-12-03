f55ed1d84d10e2dd2eccf4d14e7941a1c4bb0afd
Alessio Ragno <alessio@Alessio-HP.rcmd.it>
2019-12-03 18:03:24 +0100

    Add pong ram

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 
 	env = gym.make(env_name)
 
-	if env_name in ['MountainCar-v0', 'CartPole-v0', 'Acrobot-v1', 'LunarLander-v2']:
+	if env_name in ['MountainCar-v0', 'CartPole-v0', 'Acrobot-v1', 'LunarLander-v2', 'Pong-ram-v0']:
 		policy_model = nn_model(env.observation_space.shape, env.action_space.n)
 	elif env_name == 'Pong-v0':
 		policy_model = nn_model(env.observation_space.shape, env.action_space.n, convolutional=True)
